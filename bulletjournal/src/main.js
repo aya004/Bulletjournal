e3a43c10e20008d4aa292a2eda7e1779271a554d
aya004 <aya.aleiti004@gmail.com>
2022-04-16 02:40:07 +0200

    installing fontawesome packeges

--- bulletjournal/src/main.js
+++ bulletjournal/src/main.js
@@ -3,7 +3,15 @@ import App from './App.vue'
 import router from './router'
 import VueSidebarMenu from 'vue-sidebar-menu'
 import 'vue-sidebar-menu/dist/vue-sidebar-menu.css'
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas)
+import { fab } from '@fortawesome/free-brands-svg-icons'
+library.add(fab)
+import { far } from '@fortawesome/free-regular-svg-icons'
+library.add(far)
 
 createApp(App).use(router).mount('#app')
 App.use(VueSidebarMenu)
+app.component("font-awesome-icon", FontAwesomeIcon)
 App.mount("#app")
