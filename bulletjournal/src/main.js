2fa448e25a74ccf6da17f1406957c3e508e43f72
aya004 <aya.aleiti004@gmail.com>
2022-04-16 02:26:39 +0200

    vue-sidebar-menu installation

--- bulletjournal/src/main.js
+++ bulletjournal/src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueSidebarMenu from 'vue-sidebar-menu'
+import 'vue-sidebar-menu/dist/vue-sidebar-menu.css'
 
 createApp(App).use(router).mount('#app')
+App.use(VueSidebarMenu)
+App.mount("#app")
